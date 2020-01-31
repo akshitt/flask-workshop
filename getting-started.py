a72e607a30226fd7ccfb89be92df785ec1a99dc4
akshit <akshit@rsalesarm.com>
2020-01-31 15:42:55 +0530

    some content added

--- getting-started.py
+++ getting-started.py
@@ -1,9 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash
+from forms import RegistrationForm, LoginForm 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
 
 @app.route("/")
-def hello():
-	return "Kudos! You are done for now."
+def home():
+	return render_template('home.html')
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+
+	return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	
+	if form.validate_on_submit():
+		
+		if form.username.data == 'akshit' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')	
+	return render_template('login.html', form=form)
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+# 127.0.0.1 is the IP address of your local machine. 5000 is the port number. 
+# localhost instead of 127.0.0.1 will give the same results.
